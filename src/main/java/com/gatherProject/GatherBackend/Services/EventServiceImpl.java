75332e367c029154dcccd2309e77d430ccbbcd35
Andrew Strain <andrew.d.strain@outlook.com>
2020-11-12 17:26:16 -0500

    Remove unused import

--- src/main/java/com/gatherProject/GatherBackend/Services/EventServiceImpl.java
+++ src/main/java/com/gatherProject/GatherBackend/Services/EventServiceImpl.java
@@ -5,7 +5,6 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
